e20dc2d567a064328876f3064de9fa104cf2e442
Unknown <cabralrodrigoariel@gmail.com>
2017-04-21 17:22:39 -0300

    Se crea el javaDoc de la clase Elfo

--- src/main/java/dominio/Elfo.java
+++ src/main/java/dominio/Elfo.java
@@ -1,7 +1,20 @@
 package dominio;
 
+/**
+ * Clase que define las caracteristicas y habilidades 
+ * de el tipo de personaje "Elfo"
+ */
+
 public class Elfo extends Personaje {
 
+	/**
+	 * Método que define el nombre, la casta y el id del
+	 * personaje de tipo "Elfo"
+	 * @param nombre de tipo string, es el nombre de un personaje creado
+	 * @param casta de tipo Casta, es la casta a la que pertenece el personaje
+	 * @param id de tipo int, define el id del personaje
+	 */
+	
 	public Elfo(String nombre, Casta casta, int id) {
 		super(nombre, casta, id);
 		energiaTope += 10;
@@ -13,7 +26,30 @@ public class Elfo extends Personaje {
 		habilidadesRaza[0] = "Golpe Level";
 		habilidadesRaza[1] = "Ataque Bosque";
 	}
-
+    
+	/**
+	 * Método que define las habilidades de la raza de personaje "Elfo",
+	 * no tiene retorno
+	 * @param nombre de tipo string, nombre de un personaje creado
+	 * @param salud de tipo int, cantidad de salud del personaje al que se le 
+	 * quiere determinar su habilidad.
+	 * @param energia de tipo int, cantidad de energia  del personaje al que se le 
+	 * quiere determinar su habilidad.
+	 * @param fuerza de tipo int, cantidad de fuerza  del personaje al que se le 
+	 * quiere determinar su habilidad.
+	 * @param destreza de tipo int, cantidad de destreza  del personaje al que se le 
+	 * quiere determinar su habilidad.
+	 * @param inteligencia de tipo int, cantidad de inteligencia del personaje al que se le 
+	 * quiere determinar su habilidad.
+	 * @param casta de tipo Casta, corresponde a el tipo de casta del personaje.
+	 * @param experiencia de tipo int, es la cantidad de experiencia  del personaje al que se le 
+	 * quiere determinar su habilidad.
+	 * @param nivel de tipo int, corresponde a el nivel del personaje al que se le 
+	 * quiere determinar su habilidad.
+	 * @param idPersonaje de tipo int, nivel del personaje al que se le 
+	 * quiere determinar su habilidad.
+	 */
+	
 	public Elfo(String nombre, int salud, int energia, int fuerza, int destreza, int inteligencia, Casta casta,
 			int experiencia, int nivel,
 			int idPersonaje) {
@@ -26,7 +62,12 @@ public class Elfo extends Personaje {
 		habilidadesRaza[1] = "Ataque Bosque";
 	}
 
-	// Golpe Level
+	/**
+	 * Método que determina el resultado de una pelea
+	 * que utilice la habilidad "Golpe Level".
+	 * @param atacado : De tipo Peleable, es el personaje que recibe el ataque.
+	 * @return boolean : Retorna verdadero o falso dependiendo de la cantidad de energia
+	 */
 	public boolean habilidadRaza1(Peleable atacado) {
 		if (this.getEnergia() > 10) {
 			this.setEnergia(this.getEnergia() - 10);
@@ -36,7 +77,12 @@ public class Elfo extends Personaje {
 		return false;
 	}
 
-	// Ataque Bosque
+	/**
+	 * Método que determina el resultado de una pelea
+	 * que utilice la habilidad "Ataque Bosque".
+	 * @param atacado : De tipo Peleable, es el personaje que recibe el ataque.
+	 * @return boolean : Retorna verdadero o falso dependiendo de la cantidad de energia
+	 */
 	public boolean habilidadRaza2(Peleable atacado) {
 		if (this.getEnergia() > 10) {
 			this.setEnergia(this.getEnergia() - 10);
