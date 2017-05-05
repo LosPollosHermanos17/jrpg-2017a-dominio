a61a75e38a09c0e72917577ecfc33cdf5e37b7c2
Carlos Tomalino <c.ctomalino@gmail.com>
2017-05-05 20:37:08 -0300

    * Se agregó Javadoc a Orco y MyRandom * Se agregaron Test a Orco y MyRandom

--- src/main/java/dominio/Orco.java
+++ src/main/java/dominio/Orco.java
@@ -1,11 +1,11 @@
 package dominio;
 /**
- * Define las caracteristicas y habilidades de un personaje Orco
- * 
+ *Define las caracteristicas y habilidades de un personaje Orco.
  * @since 21/04/2017
  * @version 1.0
  */
 public class Orco extends Personaje {
+
 
 	/**
 	 * Asigna un objeto Orco y lo inicializa de acuerdo a los parámetros recibidos
@@ -18,7 +18,7 @@ public class Orco extends Personaje {
 	}
 
 	/**
-	 * Asigna un objeto Orco y lo inicializa de acuerdo a los parámetros recibidos
+	 * Asigna un objeto Orco con los parámetros recibidos.
 	 * @param nombre : Nombre del personaje.
 	 * @param salud : Cantidad de salud inicial del personaje [0 a 100].
 	 * @param energia : Cantidad de energia inicial del personaje [0 a 100].
@@ -29,37 +29,36 @@ public class Orco extends Personaje {
 	 * @param experiencia : Cantidad de experiencia inicial del personaje.
 	 * @param nivel : Nivel inicial del personaje.
 	 * @param idPersonaje : Identificador único del personaje
-	 */	
+	 */
 	public Orco(String nombre, int salud, int energia, int fuerza, int destreza, int inteligencia, Casta casta,
 			int experiencia, int nivel, int idPersonaje) {
 		super(nombre, salud, energia, fuerza, destreza, inteligencia, casta, experiencia, nivel, idPersonaje,"Orco",new String[]{"Golpe Defensa","Mordisco de Vida"});
-	}
-	
+	}	
 	/**
-	 * Permite atacar a otro personaje usando el golpe defensa 
+	 * Permite atacar a otro personaje usando el golpe defensa.
 	 * @param atacado : Es el personaje que recibe el ataque.
 	 * @return True si se pudo atacar al otro personaje.
 	 */
 	public boolean habilidadRaza1(Peleable atacado) {
 		if (this.getEnergia() > 10) {
 			this.setEnergia(this.getEnergia() - 10);
-			if (atacado.serAtacado(this.getDefensa() * 2) > 0)
+			if (atacado.serAtacado(this.getDefensa() * 2) > 0) {
 				return true;
+			}
 		}
 		return false;
 	}
-	
 	/**
-	 * Permite atacar a otro personaje usando el mordisco de vida
+	 * Permite atacar a otro personaje usando el mordisco de vida.
 	 * @param atacado : Es el personaje que recibe el ataque.
 	 * @return True si se pudo atacar al otro personaje.
 	 */
 	public boolean habilidadRaza2(Peleable atacado) {
 		if (this.getEnergia() > 10) {
 			this.setEnergia(this.getEnergia() - 10);
-			int daño_causado = atacado.serAtacado(this.getFuerza());
-			if (daño_causado > 0) {
-				this.serCurado(daño_causado);
+			int daño = atacado.serAtacado(this.getFuerza());
+			if (daño > 0) {
+				this.serCurado(daño);
 				return true;
 			}
 		}
--- src/test/java/tests_dominio/TestOrco.java
+++ src/test/java/tests_dominio/TestOrco.java
@@ -16,11 +16,20 @@ public class TestOrco {
 		Humano h = new Humano("Nicolas", new Guerrero(), 1);
 		Orco o = new Orco("Hernan", new Guerrero(), 1);
 
+		Assert.assertTrue(h.getSalud() == 105); 
+		o.habilidadRaza1(h);
+		Assert.assertTrue(h.getSalud() == 95); 
+		
+	}
+	
+	@Test
+	public void testNoGolpeDefensivo() {
+		Humano h = new Humano("Nicolas", new Guerrero(), 1);
+		Orco o = new Orco("Hernan", new Guerrero(), 1);
+		o.setEnergia(5);
+		Assert.assertTrue(h.getSalud() == 105); 
+		o.habilidadRaza1(h);
 		Assert.assertTrue(h.getSalud() == 105);
-		if (o.habilidadRaza1(h))
-			Assert.assertTrue(h.getSalud() == 95);
-		else
-			Assert.assertTrue(o.getSalud() == 105);
 	}
 
 	@Test
@@ -30,12 +39,37 @@ public class TestOrco {
 
 		Assert.assertTrue(h.getSalud() == 100);
 		o.setSalud(100);
-		if (o.habilidadRaza2(h)) {
-			Assert.assertEquals(40, h.getSalud());
-			Assert.assertTrue(o.getSalud() == 100);
-		} else {
-			Assert.assertTrue(o.getSalud() == 100);
-			Assert.assertTrue(h.getSalud() == 100);
-		}
+		o.habilidadRaza2(h);
+		Assert.assertEquals(40, h.getSalud());
+		Assert.assertTrue(o.getSalud() == 100);
+		
+		
+		
 	}
+	@Test
+	public void testNoMordiscoDeVida() {
+		Humano h = new Humano("Nico", 100, 100, 55, 20, 30, new Hechicero(0.2, 0.3, 1.5), 0, 1, 1);
+		Orco o = new Orco("Nico", 100, 100, 80, 20, 30, new Asesino(0.2, 0.3, 1.5), 0, 1, 1);
+
+		Assert.assertTrue(h.getSalud() == 100);
+		o.setSalud(100);
+		o.habilidadRaza2(h);
+	
+		Assert.assertTrue(o.getSalud() == 100);
+		//Assert.assertTrue(h.getSalud() == 100);	
+	}
+	
+	@Test  
+	  public void habilidadesTest() {  
+	    Orco orco = new Orco("monstruo1", 50, 30, 200, 100, 40, new Guerrero(), 3000, 4, 444);  
+	    Orco orcoSinEnergia = new Orco("monstruo2", 300, 5, 1000, 100, 40, new Guerrero(), 3000, 4, 222);  
+	    Orco orcoSinFuerzaNiDestreza = new Orco("monstruo1", 50, 30, 0, 0, 40, new Guerrero(), 3000, 4, 4434); 
+	    Assert.assertTrue(orco.habilidadRaza1(orcoSinEnergia));  
+	    Assert.assertFalse(orcoSinEnergia.habilidadRaza1(orco));  
+	    Assert.assertTrue(orco.habilidadRaza2(orcoSinEnergia));  
+	    Assert.assertFalse(orcoSinEnergia.habilidadRaza2(orco));  
+	    Assert.assertFalse(orcoSinFuerzaNiDestreza.habilidadRaza2(orco));
+	    Assert.assertFalse(orcoSinFuerzaNiDestreza.habilidadRaza2(orco));
+	  }  
+
 }
