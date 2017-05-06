0205f9667c5ff0d5c61d5fcce6fdda3d632ec40b
Cabral Rodrigo <cabralrodrigoariel@gmail.com>
2017-05-06 04:15:39 -0300

    Se corrigen javadocs de Casta

--- src/main/java/dominio/Casta.java
+++ src/main/java/dominio/Casta.java
@@ -3,7 +3,7 @@ package dominio;
 import java.io.Serializable;
 
 /**
- * Define las caracteristicas y habilidades de un objeto Casta
+ * Define las caracteristicas y habilidades de un objeto Casta.
  * @since 21/04/2017
  * @version 1.0
  */
@@ -16,16 +16,19 @@ public abstract class Casta implements Serializable {
     protected String[] habilidadesCasta;
 
     /**
-     * Asigna un objeto Hechicero y lo inicializa
+     * Asigna un objeto Hechicero y lo inicializa.
      */
     public Casta() {
-        this.probabilidadGolpeCritico = 0.2;
-        this.probabilidadEvitarDanio = 0.2;
-        this.danioCritico = 1.5;
+        final double gp = 0.2;
+        final double ed = 0.2;
+        final double dc = 1.5;
+    	this.probabilidadGolpeCritico = gp;
+        this.probabilidadEvitarDanio = ed;
+        this.danioCritico = dc;
     }
 
     /**
-     * Asigna un objeto Casta y lo inicializa de acuerdo a los parámetros recibidos
+     * Asigna un objeto Casta y lo inicializa de acuerdo a los parámetros recibidos.
      * @param probCritico : Probabilidad de golpe critico.
      * @param evasion : Valor de evasion.
      * @param danio : Valor del daño crítico.
@@ -38,47 +41,60 @@ public abstract class Casta implements Serializable {
 
     /**
      * Aplica habilidad 1 de un personaje a otro.
+     * @param caster : Personaje atacante.
+     * @param atacado : Personaje atacado.
+     * @return true si pudo usar habilidad.
      */
     public abstract boolean habilidad1(Personaje caster, Peleable atacado);
 
     /**
      * Aplica habilidad 2 de un personaje a otro.
+     * @param caster : Personaje atacante.
+     * @param atacado : Personaje atacado.
+     * @return true si pudo usar habilidad.
      */
     public abstract boolean habilidad2(Personaje caster, Peleable atacado);
 
     /**
      * Aplica habilidad 3 de un personaje a otro.
+     * @param caster : Personaje atacante.
+     * @param atacado : Personaje atacado.
+     * @return true si pudo usar habilidad.
      */
     public abstract boolean habilidad3(Personaje caster, Peleable atacado);
 
     /**
-     * Inicializa el personaje
+     * Inicializa el personaje.
+     * @param p : Personaje a inicializar.
      */
     public abstract void inicializarPersonaje(Personaje p);
 
     /**
-     * Devuelve el nombre casta
+     * Devuelve el nombre casta.
+     * @return nombreCasta : Nombre de la casta.
      */
     public String getNombreCasta() {
         return this.nombreCasta;
     }
 
     /**
-     * Devuelve el nombre de las habilidades
+     * Devuelve el nombre de las habilidades.
+     * @return habilidadesCasta : Habilidades de la casta.
      */
     public String[] getHabilidadesCasta() {
         return habilidadesCasta;
     }
 
     /**
-     * Devuelve la probabilidad de golpe crítico
+     * Devuelve la probabilidad de golpe crítico.
+     * @return probabilidadDeGolpeCritico
      */
     public double getProbabilidadGolpeCritico() {
         return probabilidadGolpeCritico;
     }
 
     /**
-     * Setea la probabilidad de golpe crítico
+     * Setea la probabilidad de golpe crítico.
      * @param probabilidadGolpeCritico : Valor que indica la probabilidad de golpe crítico.
      */
     public void setProbabilidadGolpeCritico(final double probabilidadGolpeCritico) {
@@ -86,14 +102,15 @@ public abstract class Casta implements Serializable {
     }
 
     /**
-     * Setea la probabilidad de evitar daño
+     * Setea la probabilidad de evitar daño.
+     * @return probabilidadEvitarDanio.
      */
     public double getProbabilidadEvitarDaño() {
         return probabilidadEvitarDanio;
     }
 
     /**
-     * Setea la probabilidad de evitar daño
+     * Setea la probabilidad de evitar daño.
      * @param probabilidadEvitarDanio : Valor que indica la probabilidad de evitar daño.
      */
     public void setProbabilidadEvitarDaño(final double probabilidadEvitarDanio) {
@@ -101,7 +118,7 @@ public abstract class Casta implements Serializable {
     }
 
     /**
-     * Setea el valor del daño crítico
+     * Setea el valor del daño crítico.
      * @param danioCritico : Valor que indica el daño crítico
      */
     public void setDañoCritico(final double danioCritico) {
@@ -109,7 +126,8 @@ public abstract class Casta implements Serializable {
     }
 
     /**
-     * Devuelve el daño crítico
+     * Devuelve el daño crítico.
+     * @return danioCritico.
      */
     public double getDañoCritico() {
         return this.danioCritico;
